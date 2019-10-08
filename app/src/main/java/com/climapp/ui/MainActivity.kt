8cf9c919eeb525fdb776ac6d6615457fade447f7
Gaston Saillen <gaston_luque_29@hotmail.com>
2019-10-08 08:45:58 -0300

    Primer commit

---
+++ app/src/main/java/com/climapp/ui/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.climapp.ui
+
+import android.location.LocationProvider
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.climapp.R
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var fusedLocationProvider: FusedLocationProviderClient
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        fusedLocationProvider = LocationServices.getFusedLocationProviderClient(this)
+    }
+}
