41954900b56b92693ca1b18a460c81a2484e83e5
Gaston Saillen <gaston_luque_29@hotmail.com>
2019-10-08 10:27:15 -0300

    Base app

---
+++ app/src/main/java/com/climapp/model/Clima.kt
@@ -0,0 +1,6 @@
+package com.climapp.model
+
+/**
+ * Created by Gastón Saillén on 08 October 2019
+ */
+data class Clima(var time:Double = 0.0,var summary:String = "DEFAULT", var icon:String = "DEFAULT",var temperature:Double = 0.0)
