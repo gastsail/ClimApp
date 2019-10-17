8e5f696b3b9dcc38fdd17ceed8c9eca5deb1e49e
Gaston Saillen <gaston_luque_29@hotmail.com>
2019-10-17 17:53:35 -0300

    Base app no architecture

--- app/src/main/java/com/climapp/ui/MainActivity.kt
+++ app/src/main/java/com/climapp/ui/MainActivity.kt
@@ -8,17 +8,17 @@ import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 import androidx.core.app.ActivityCompat
 import android.content.pm.PackageManager
-import android.util.Log
+import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import com.dezlum.codelabs.getjson.GetJson
 import com.climapp.model.Clima
-import com.dezlum.codelabs.getjson.GetJson.convertInputStreamToString
 import com.google.gson.Gson
-import com.google.gson.JsonObject
-import java.io.InputStream
-import java.net.HttpURLConnection
-import java.net.URL
+import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.time.format.TextStyle
+import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -26,7 +26,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
     private var latitud: Double = -31.43294
     private var longitud: Double = -64.2768856
-    var apiUrl = "https://api.darksky.net/forecast/6f7e254206a8285a9e7b0506af425ef9/$latitud,$longitud"
+    var apiUrl = "https://api.darksky.net/forecast/6f7e254206a8285a9e7b0506af425ef9/$latitud,$longitud?lang=es"
 
 
     companion object {
@@ -70,14 +70,38 @@ class MainActivity : AppCompatActivity() {
             if (location != null) {
                 latitud = location.latitude
                 longitud = location.longitude
-                txt_test_latlong.text = "Latitud $latitud , Longitud: $longitud"
-                val jsonObject = GetJson().AsJSONObject(apiUrl).get("currently")
+
+                val currentObj = GetJson().AsJSONObject(apiUrl).get("currently")
+                val mainDataObj = GetJson().AsJSONObject(apiUrl)
+                val dayObj = mainDataObj.getAsJsonObject("daily").getAsJsonArray("data").get(0)
+
                 val gson = Gson()
-                val clima = gson.fromJson(jsonObject,Clima::class.java)
-                val temperaturaEnGrados = (clima.temperature - 32) * 5/9
-                txt_test_latlong.text = "La temperatura actual es de ${roundToDecimals(temperaturaEnGrados,2)} ºC y esta ${clima.summary}"
+                val datosClima = gson.fromJson(currentObj,Clima::class.java)
+                val dayClima = gson.fromJson(dayObj,Clima::class.java)
+                val datosGenerales = gson.fromJson(mainDataObj,Clima::class.java)
+                val temperaturaEnGrados = (datosClima.temperature - 32) * 5/9
+
+                val date = SimpleDateFormat("dd/MM/yyyy").format(Date(dayClima.time*1000))
+
+                tvCurrentDayName.text = getDayOfWeek(date)
+                tvCurrentCity.text = datosGenerales.timezone
+                tvCurrentWeatherTemperature.text = roundToDecimals(temperaturaEnGrados,2).toString()+" ºC"
+                tvCurrentWeatherSummary.text = datosClima.summary
+                tvMaxAndMin.text = "max: ${(roundToDecimals((dayClima.temperatureHigh - 32) * 5/9,2))} ºC  min: ${(roundToDecimals((dayClima.temperatureLow - 32) * 5/9,2))} ºC"
+
+                progressBar.visibility = View.GONE
+                relativeClima.visibility = View.VISIBLE
+                relativeRecycler.visibility = View.VISIBLE
+                tvDatosClima.visibility = View.GONE
+
             }
         }
+    }
+
+    fun getDayOfWeek(date: String): String {
+        val locale = Locale("es", "ES")
+        return LocalDate.parse(date, DateTimeFormatter.ofPattern("dd/MM/yyyy")).getDayOfWeek().getDisplayName(
+            TextStyle.FULL,locale)
     }
 
     fun roundToDecimals(number: Double, numDecimalPlaces: Int): Double {
